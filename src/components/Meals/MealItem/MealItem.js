23e8e46f8e9363b49b846df686f8815dd2d11e76
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-28 17:03:18 -0500

    added styling to meal list and a new component

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,17 @@
+import classes from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const price = `$ ${props.price.toFixed(2)}`;
+  return (
+    <li className={classes.meal}>
+      <div className={classes.description}>
+        <h3>{props.name}</h3>
+        <p>{props.description}</p>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
