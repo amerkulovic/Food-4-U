f55276064d9244a100cebcf9a167426b00d2a06c
Amer Kulovic <amer.kulovic@yahoo.com>
2023-03-28 14:21:31 -0500

    created CartIcon and Button components

---
+++ src/components/Layout/CartButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CartButton = (props) => {
+  return (
+    <button>
+      <span>
+        
+      </span>
+      <span></span>
+      <span></span>
+    </button>
+  );
+};
+
+export default CartButton;
